b0459772c7be20bb0c393135f17a912c0c83055c
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-03 19:11:26 +0530

    Employee Attendence

--- src/employeewage.java
+++ src/employeewage.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 public class employeewage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        Random ran = new Random();
+        int attendance = ran.nextInt(2);
+        {
+            if (attendance == 1)
+                System.out.println("Employee is Present");
+            else
+                System.out.println("Employee is Absent");
+        }
     }
 }
