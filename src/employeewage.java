0c06eeece701412665dfaacbe08a43a3ff26353b
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-03 20:07:03 +0530

    Employee Monthly Wage

--- src/employeewage.java
+++ src/employeewage.java
@@ -4,6 +4,7 @@ public class employeewage {
     static int Wage_per_Hour = 20;
     static int Full_Day_Hour = 8;
     static int Part_Time_Hour = 4;
+    static int Working_Days_per_Month = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
@@ -14,11 +15,13 @@ public class employeewage {
             System.out.println("Employee is Present");
             System.out.println("Employee is Full time");
             System.out.println("Employee`s Daily Wage is => " + Wage_per_Hour * Full_Day_Hour);
+            System.out.println("Employee`s Monthly Wage is => " + Wage_per_Hour * Full_Day_Hour * Working_Days_per_Month);
 
         } else if (attendance == 2) {
             System.out.println("Employee is Present");
             System.out.println("Employee is Part time");
             System.out.println("Employee`s Daily Wage is => " + Wage_per_Hour * Part_Time_Hour);
+            System.out.println("Employee`s Monthly Wage is => " + Wage_per_Hour * Part_Time_Hour * Working_Days_per_Month);
 
         } else {
             System.out.println("Employee is Absent");
--- src/emplyoyeewageswitchcase.java
+++ src/emplyoyeewageswitchcase.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class emplyoyeewageswitchcase {
+import java.util.Random;
+
+public class emplyoyeewageswitchcase {
+    static int Wage_per_Hour = 20;
+    static int Full_Day_Hour = 8;
+    static int Part_Time_Hour = 4;
+    static int Working_Days_per_Month = 20;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        Random ran = new Random();
+        int attendance = ran.nextInt(3);
+
+        switch (attendance) {
+            case 1 -> {
+                System.out.println("Employee is Present");
+                System.out.println("Employee is Full time");
+                System.out.println("Employee`s Daily Wage is => " + Wage_per_Hour * Full_Day_Hour);
+                System.out.println("Employee`s Monthly Wage is => " + Wage_per_Hour * Full_Day_Hour * Working_Days_per_Month);
+            }
+            case 2 -> {
+                System.out.println("Employee is Present");
+                System.out.println("Employee is Part time");
+                System.out.println("Employee`s Daily Wage is => " + Wage_per_Hour * Part_Time_Hour);
+                System.out.println("Employee`s Monthly Wage is => " + Wage_per_Hour * Part_Time_Hour * Working_Days_per_Month);
+            }
+            default -> System.out.println("Employee is Absent");
+        }
+    }
 }
